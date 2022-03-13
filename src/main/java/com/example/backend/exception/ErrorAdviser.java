12e30dab860120426524adb90ebd4613acb334f9
pakkawat <pakkawat.khotarut@gmail.com>
2022-03-14 06:26:58 +0700

    Update ErrorAdviser.java

--- src/main/java/com/example/backend/exception/ErrorAdviser.java
+++ src/main/java/com/example/backend/exception/ErrorAdviser.java
@@ -1,6 +1,5 @@
 package com.example.backend.exception;
 
-import com.example.backend.exception.BaseException;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
