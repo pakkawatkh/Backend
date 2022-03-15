2c0ffd0c8e77f9f048e9dcdb2e170e71afd97fa8
pakkawat <pakkawat.khotarut@gmail.com>
2022-03-15 08:01:36 +0700

    Delete NewsApi.java

--- src/main/java/com/example/backend/api/NewsApi.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.api;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/news")
-public class NewsApi {
-}
